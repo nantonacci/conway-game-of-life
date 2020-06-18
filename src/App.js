8ed5767e8ec25c925e37dcfd83ee5a668667fe25
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-06-17 20:22:45 -0700

    :D

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Universe from './components/Universe.js';
 
-import './App.css';
+import './App.scss';
 
 export default class App extends Component {
 
