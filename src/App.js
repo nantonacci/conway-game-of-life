e3640b5b6b551296acae0268dbfbc9c277f90152
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-06-22 21:12:43 -0700

    Super cool vaporwave styles added.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ export default class App extends Component {
       universe: new Universe(),
       size: [25,25],
       gameRunning: false,
-      speed: 1000
+      speed: 1000,
+      random: false,
+      color: "#0000FF"
     }
 
     this.handleColumnChange = this.handleColumnChange.bind(this);
@@ -21,7 +23,10 @@ export default class App extends Component {
     this.stopGame = this.stopGame.bind(this);
     this.resetGame = this.resetGame.bind(this);
     this.renderBoard = this.renderBoard.bind(this);
+    this.randomize = this.randomize.bind(this);
     this.storeCell = this.storeCell.bind(this);
+    this.changeColor = this.changeColor.bind(this);
+    this.romanize = this.romanize.bind(this);
   }
   
   runGame() {
@@ -88,7 +93,6 @@ export default class App extends Component {
     }
   }
 
-
   stopGame(){
     console.log('stopped game')
     this.setState({
@@ -120,13 +124,60 @@ export default class App extends Component {
     }
   }
 
+  changeColor(e){
+    console.log('changed color to ', e.target.value)
+    this.setState({
+        color: e.target.value
+      })
+    
+  }
+
   renderBoard() {
     let newWorld = [];
     let cellRow = [];
 
+    if (this.state.random == false){
+      console.log('render board!')
+      for(let i = 0; i < this.state.size[0]; i++) {
+        for (let j = 0; j < this.state.size[1]; j++){
+          if(this.state.universe.isCellAlive(i + " , " + j)){
+            cellRow.push(
+              <Cell color={this.state.color} key={[i, j]} position={{x: i, y: j}} live={true} storeCell={this.storeCell.bind(this)}/>
+            );
+          } else {
+            cellRow.push(
+              <Cell color={"#deffeb"} key={[i, j]} position={{x: i, y: j}} live={false} storeCell={this.storeCell.bind(this)}/>
+            );
+          }
+        }
+        newWorld.push(<div className="row" key={i}>{cellRow}</div>);
+        cellRow = [];
+      }
+      return newWorld;
+    } else {
+      console.log('random board timeout!')
+      setTimeout(() => {
+        this.setState({
+        random: false
+        })
+      }, 100)
+      // setTimeout(() => {
+        
+      // }, 200)
+      return this.randomBoard()
+    }
+    
+  }
+
+
+  randomBoard() {
+    let newWorld = [];
+    let cellRow = [];
+    console.log('random board!')
     for(let i = 0; i < this.state.size[0]; i++) {
       for (let j = 0; j < this.state.size[1]; j++){
-        if(this.state.universe.isCellAlive(i + " , " + j)){
+        let randomNum = Math.random()
+        if(randomNum > .4){
           cellRow.push(
             <Cell key={[i, j]} position={{x: i, y: j}} live={true} storeCell={this.storeCell.bind(this)}/>
           );
@@ -143,43 +194,70 @@ export default class App extends Component {
     return newWorld;
   }
 
+  randomize() {
+    console.log('random go!')
+    if (!this.state.random){
+      this.setState({
+        random: true
+      })
+    }
+  }
+
+  romanize() {
+    let num = this.state.universe.getGeneration()
+    let lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+    for ( i in lookup ) {
+      while ( num >= lookup[i] ) {
+        roman += i;
+        num -= lookup[i];
+      }
+    }
+    return roman;
+  }
+
   render(){
     return (
 
-        <div className="worldContainer">
-          <div className="headerContainer">
-            <div className="headerInnerContainer">
+        <div className="container">
+          <div className="header">
+            <div className="header-labels">
 
-              <label className="label">
+              <label>
                 Rows:
                 <input className="input" type="text" value={this.state.size[1]} onChange={this.handleRowChange} />
               </label>
 
-              <label className="label">
+              <label>
                 Columns:
                 <input className="input" type="text" value={this.state.size[0]} onChange={this.handleColumnChange} />
               </label>
 
-              <label className="label">
+              <label>
                 Change speed:
                 <input className="input" type="text" value={this.state.speed} onChange={(e) => {this.changeSpeed(e)}} />
               </label>
 
+              <label>
+                Change square color:
+                <input className="input" type="text" value={this.state.color} onChange={(e) => {this.changeColor(e)}} />
+              </label>
             </div>
 
             <div className="headerButtons">
-              <button className="submit" onClick={this.startGame}>Start</button>
-              <button className="submit" onClick={this.stopGame}>Stop</button>
-              <button className="submit" onClick={this.resetGame}>Reset</button>
+              <button onClick={this.startGame}>Start</button>
+              <button onClick={this.stopGame}>Stop</button>
+              <button onClick={this.resetGame}>Reset</button>
+              <button onClick={this.randomize}>Randomize</button>
             </div>
 
-            Generation: {this.state.universe.getGeneration()}
+            
           </div>
 
-          <div className="boardContainer">
+          <div className="board">
             {this.renderBoard()}
+            
           </div>
-
+          <p className="gen">Generation: {this.romanize()}</p>
         </div>
 
     );
@@ -189,7 +267,7 @@ export default class App extends Component {
 class Cell extends Component {
   render() {
     return (
-      <div onClick={() => this.props.storeCell(this.props.position)} className={this.props.live ? "cellContainerLive" : "cellContainerDead"}></div>
+      <div style={{backgroundColor: this.props.color}} onClick={() => this.props.storeCell(this.props.position)} className={this.props.live ? "cellContainerLive" : "cellContainerDead"}></div>
     );
   }
 }
